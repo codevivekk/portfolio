fd02d0c51d2bd58d9fa887b1e4039fb8bb97ffe3
Vivek Kushwaha <codevivekk@gmail.com>
2025-01-10 16:04:41 +0530

    push

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,3 @@
-"use client"
 import dynamic from "next/dynamic";
 import Experience from "@/components/Experience/Experience";
 import HomeSection from "@/components/HomeSection/HomeSection";
@@ -7,29 +6,18 @@ import Review from "@/components/Review/Review";
 import SelectedProjects from "@/components/SelectedProjects/SelectedProjects";
 import Skills from "@/components/skills/Skills";
 import SocialMedia from "@/components/socialMedia/SocialMedia";
-import { useEffect } from "react";
-
 export default function Home() {
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      window.scrollTo({
-        top: document.body.scrollHeight,
-        behavior: "smooth",
-      });
-    }
-  }, []);
 
 
   return (
     <main className="text-white">
-      <SocialMedia />
-      <Review/>
-      <Experience/>
-      <Skills/>
-      <SelectedProjects/>
+      {/* <HomeSection/> */}
       <MyInfo/>
-      <HomeSection/>
+      <SelectedProjects/>
+      <Skills/>
+      <Experience/>
+      <Review/>
+      <SocialMedia />
     </main>
   );
 }
--- src/components/HomeSection/HomeSection.jsx
+++ src/components/HomeSection/HomeSection.jsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import gsap from "gsap";
 import Image from "next/image";
-import { mountains_front } from "../../../public/assets/parallex";
+import { moon, mountains_behind, mountains_front } from "../../../public/assets/parallex";
 
 const HomeSection = () => {
 
@@ -23,26 +23,23 @@ const HomeSection = () => {
   }, []);
 
   return (
-    <div className="w-full h-[90vh] parallax-wrapper relative">
+    <div className="w-full h-screen parallax-wrapper ">
       <div className="absolute bounce-animation   inset-0 flex items-center justify-center text-white z-10 text-6xl font-bold">
-        <p>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="2"
-            stroke="currentColor"
-            className="w-20 h-20"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M19 14l-7-7m0 0l-7 7m7-7v18"
-            />
-          </svg>
-        </p>
-        <p className="text-size">Scroll Up</p>
+
+        <p className="text-size">Welcome</p>
       </div>
+
+      <Image
+        alt="mountains"
+        src={moon}
+        className="mountains_front absolute w-full h-full "
+      />
+
+      <Image
+        alt="mountains"
+        src={mountains_behind}
+        className="mountains_front  "
+      />
 
       <Image
         alt="mountains"
