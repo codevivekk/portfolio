c4409170c7b56f43cb7154ddf819c7730971ece9
Vivek Kushwaha <codevivekk@gmail.com>
2025-01-10 15:51:24 +0530

    Home Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Experience from "@/components/Experience/Experience";
-// import HomeSection from "@/components/HomeSection/HomeSection";
+import HomeSection from "@/components/HomeSection/HomeSection";
 // import MyInfo from "@/components/MyInfo/MyInfo";
 import Review from "@/components/Review/Review";
 import SelectedProjects from "@/components/SelectedProjects/SelectedProjects";
@@ -15,7 +15,7 @@ export default function Home() {
       <Skills/>
       <SelectedProjects/>
       {/* <MyInfo/> */}
-      {/* <HomeSection/> */}
+      <HomeSection/>
     </main>
   );
 }
