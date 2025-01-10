57016f3cbb16c9741d9136f43842a2940f9ebe07
Vivek Kushwaha <codevivekk@gmail.com>
2025-01-10 15:59:20 +0530

    client

--- src/app/page.js
+++ src/app/page.js
@@ -7,9 +7,10 @@ import Review from "@/components/Review/Review";
 import SelectedProjects from "@/components/SelectedProjects/SelectedProjects";
 import Skills from "@/components/skills/Skills";
 import SocialMedia from "@/components/socialMedia/SocialMedia";
+import { useEffect } from "react";
 
 export default function Home() {
-  
+
   useEffect(() => {
     if (typeof window !== "undefined") {
       window.scrollTo({
