c0ebb911929155db5e4aef030832a28bad98a59c
vivek <codevivekk@gmail.com>
2026-03-19 22:27:43 +0530

    feat: Implement initial portfolio website structure, core components, and themeing.

--- public/assets/icons/index.js
+++
@@ -1,1 +0,0 @@
-export {default as coding } from "./coding.json"
--- public/assets/parallex/index.js
+++
@@ -1,4 +0,0 @@
-export { default as moon } from "./moon.png";
-export { default as mountains_behind } from "./mountains_behind.png";
-export { default as mountains_front } from "./mountains_front.png";
-export { default as stars } from "./stars.png";
